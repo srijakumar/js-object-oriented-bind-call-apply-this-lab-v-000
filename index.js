c85837357b71d442ddd6a75867342b26ec83d90f
Srija S <srijakumar7@gmail.com>
2018-02-25 20:23:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 function justInvoke(fn){
   return fn();
 }
+
+function setThisWithCall(fn, thisValue,  arg){
+  return fn.call(thisValue,argument)
+}
+
+function setThisWithApply(fn, thisValue, args){
+  return fn.apply(thisValue, args)
+}
